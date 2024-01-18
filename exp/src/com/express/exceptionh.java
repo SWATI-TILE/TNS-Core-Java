3e37993721bdfa76b46a108e4dede508d4b14fce
Admin <Admin@DESKTOP-IAECP3C>
2024-01-18 21:53:52 +0530

    constructor practice

---
+++ exp/src/com/express/exceptionh.java
@@ -0,0 +1,25 @@
+package com.express;
+
+public class exceptionh {
+	static int a;
+	static {
+		a=4;
+		System.out.println("inside");
+		System.out.println("a= "+a);
+	}
+	exceptionh()
+	{
+		System.out.println("inside constructor");
+		a=10;
+	}
+	public static void func()
+	{
+		a=a+1;
+		System.out.println("a= "+a);
+	}
+	public static void main(String[] args) {
+		exceptionh ob=new exceptionh();
+		ob.func();
+	}
+
+}
